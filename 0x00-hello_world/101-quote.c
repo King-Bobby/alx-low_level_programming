f1e9247afcb69b2eae1386b4c8427998b2eff1de
Noel Brown <noelbrowndboss@gmail.com>
2022-12-08 09:52:29 -0800

    expanding with printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - %s %s, %s-%s-%s\n", "Dora", "Korpar", "2015", "10", "19");
-	
+	printf("and that piece of art is useful\" -%s %s, %s-%s-%s\n","Dora","Korpar","2015","10","19");
 	return (1);
 }
