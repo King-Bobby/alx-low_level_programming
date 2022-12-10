303376f6d23dbba534d34fbbc548189e9512c125
Noel Brown <noelbrowndboss@gmail.com>
2022-12-09 17:03:44 -0800

    writing program using else if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,24 +10,23 @@
  */
 int main(void)
 {
-		int n;
-		int t;
+		int n, l;
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-				t = n;
-				if (n > 5)
+				l = n % 10;
+					
+				if (l > 5)
 				{
-					printf("Last digit of %d is %u greater than 5\n", n, t);
+					printf("Last digit of %d is %d and is  greater than 5\n", n, l);
 				}
-				else if (n == 0)
+				else if (l == 0)
 				{
-					printf("Last digit of %d is 0\n", n);
+					printf("Last digit of %d is %d and is 0\n", n, l);
 				}
 				else if ((n < 6) != 0)
 				{
-					printf("Last digit of %d is less than 6 and not 0\n", n);
+					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 				}
 					return (0);
 }
