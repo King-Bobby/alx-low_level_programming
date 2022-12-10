bf1e28dbd2499baa51dbe222a493948ac5c4c9c0
Noel Brown <noelbrowndboss@gmail.com>
2022-12-09 19:13:11 -0800

    testing theory

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 48;
+	b = 48;
+
+	if (a < 58)
+	{
+		b = 48;
+		while (b < 58)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == 57 && b == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
