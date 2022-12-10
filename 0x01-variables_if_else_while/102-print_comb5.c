723e749a854dce3b986df87d89e652250c86f24f
Noel Brown <noelbrowndboss@gmail.com>
2022-12-09 20:25:25 -0800

    testing advanced theory part 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,9 +16,9 @@ int main(void)
 	{
 		for (b = 48; b < 58; b++)
 		{
-			for (c = 48; c < 58; c++)
+			for (c = a; c < 58; c++)
 			{
-				for (d = 48; d < 58; d++)
+				for (d = b + 1; d < 58; d++)
 				{
 					putchar(a);
 					putchar(b);
