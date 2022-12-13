1e3bffef7eca422e965ba7efc0266c6202bbdac7
Noel Brown <noelbrowndboss@gmail.com>
2022-12-13 15:25:35 -0800

    testing

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,13 @@
 /**
  * _isalpha - return 1 if c is a letter
  *
+ * @c: The character in ASCII
+ *
  * Return: 1 for letters, 0 otherwise
  */
 int _isalpha(int c)
 {
-	if((c > 96 && c < 123) || (c > 64 && c < 91))
+	if ((c > 96 && c < 123) || (c > 64 && c < 91))
 	{
 		return (1);
 	}
