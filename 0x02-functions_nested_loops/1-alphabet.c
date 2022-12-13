571e8c09dd9760aeee299c08468ff9b0302e6934
Noel Brown <noelbrowndboss@gmail.com>
2022-12-13 13:25:04 -0800

    how to print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - main
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
