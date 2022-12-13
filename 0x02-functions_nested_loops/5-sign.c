300ea670b62c319513d22bcd34209a877bc6efce
Noel Brown <noelbrowndboss@gmail.com>
2022-12-13 15:52:44 -0800

    using print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - prints results depending on if the number is greater, less or equal to 0
+ * print_sign - print value if the number is greater, less or equal to -1
  *
  * @n: the number input
  *
@@ -9,14 +9,14 @@
  */
 int print_sign(int n)
 {
-	if (n < 0)
-	{
-		putchar(45);
-		return (-1);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar(45);
 		return (1);
 	}
 	else
