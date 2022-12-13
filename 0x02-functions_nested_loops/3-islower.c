fee66ca5b64ee49f5897bd604176a021b6274f1e
Noel Brown <noelbrowndboss@gmail.com>
2022-12-13 15:28:28 -0800

    it works

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,19 @@
 /**
  * _islower - returns 1 if c is lowercase
  *
+ * @c: The character in ASCII
+ *
  * Return: Returns 1 if c is lowercase, returns 0 otherwise
  */
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 	putchar('\n');
 }
