70e17b0e01bf504060018137f3087245254620ee
Noel Brown <noelbrowndboss@gmail.com>
2022-12-14 10:02:27 -0800

    print absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,12 +9,16 @@
  */
 int _abs(int i)
 {
-	if (i >= 0)
+	if (i > 0)
 	{
-		return (1);
+		return (i);
+	}
+	else if (i < 0)
+	{
+		return (-i);
 	}
 	else
 	{
-		return (i * -1);
+		return (0);
 	}
 }
