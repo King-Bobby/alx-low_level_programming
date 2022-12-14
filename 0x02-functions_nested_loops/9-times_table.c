cfa8eb5fc05fbcbaad06beb57567914ae8306b67
Noel Brown <noelbrowndboss@gmail.com>
2022-12-14 04:11:07 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times-tables - prints times tables
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int r;
+	int c;
+	int a;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			a = (r * c);
+
+			if (c == 0)
+			{
+				putchar('0' + a);
+			}
+			else if (a < 10)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar('0' + a);
+			}
+			else if (a > 9)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar('0' + (a / 10));
+				putchar('0' + (a % 10));
+			}
+		}
+		putchar('\n');
+	}
+}
