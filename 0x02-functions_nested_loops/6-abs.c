0b206dd9023b32b27294e4f41283c3f083bd74ec
Noel Brown <noelbrowndboss@gmail.com>
2022-12-13 16:05:57 -0800

    test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ *
+ * @i - integer value
+ *
+ * Return: Absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
