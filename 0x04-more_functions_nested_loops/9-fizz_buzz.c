a3c182e2b5832f914c8feab479ee8720e51f59d7
Noel Brown <noelbrowndboss@gmail.com>
2022-12-15 11:15:10 -0800

    FIZZBUZZ

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,22 @@ int main(void)
 		if ((a % 3) == 0 && (a % 5) == 0)
 		{
 			printf("FizzBuzz");
-			putchar(' ');
 		}
 		else if ((a % 3) == 0)
 		{
 			printf("Fizz");
-			putchar(' ');
 		}
 		else if ((a % 5) == 0)
 		{
 			printf("Buzz");
-			putchar(' ');
 		}
 		else
 		{
 			printf("%d", a);
+		}
+
+		if (a != 100)
+		{
 			putchar(' ');
 		}
 	}
