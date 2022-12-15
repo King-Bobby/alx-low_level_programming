0ca84a4bd8fb0ba07e00bbf4fab0b5d8df797f6b
Noel Brown <noelbrowndboss@gmail.com>
2022-12-15 08:38:36 -0800

    print numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,10 @@
 
 /**
  * print_numbers - prints numbers from 0 - 9
- * @n: number
  * Return: Always 0
  */
 
 void print_numbers(void)	
 {
-	int n;
-
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
+	printf("0123456789\n");
 }
