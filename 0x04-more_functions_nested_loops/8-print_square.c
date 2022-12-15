3f8b4f4daa2b8948ac57c78ce1e9f575b80f5051
Noel Brown <noelbrowndboss@gmail.com>
2022-12-15 10:21:52 -0800

    making squares on the terminal

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square -  prints a square in terminal
+ *
+ * Return: Always 0
+ */
+void print_square(int size)
+{
+	int i;
+	int a;
+	int c = 35;
+	
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (a = 1; a <= size; a++)
+			{
+				putchar(c);
+			}
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
