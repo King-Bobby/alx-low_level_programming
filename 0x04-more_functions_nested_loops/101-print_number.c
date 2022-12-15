a9e91bb17f3f270b39da632a28258990b645f39f
Noel Brown <noelbrowndboss@gmail.com>
2022-12-15 12:08:57 -0800

    use putchar to print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,5 +7,21 @@
  */
 void print_number(int n)
 {
-	printf("%d", n);
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		k = n;
+		putchar('-');
+	}
+
+	k = k / 10;
+
+	if (k != 0)
+	{
+		print_number(k);
+	}
+
+	putchar ((unsigned int) n % 10 + '0');
 }
