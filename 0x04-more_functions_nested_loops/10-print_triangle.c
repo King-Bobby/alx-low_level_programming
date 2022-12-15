c206b8979f2d8a2914a14c18939650f1866f610c
Noel Brown <noelbrowndboss@gmail.com>
2022-12-15 11:42:25 -0800

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle in terminal
+ * @size: size of triangle
+ * Return: Always 0
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+	
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= (size - a); b++)
+			{
+				putchar(' ');
+			}
+			for (c = 1; c <= a; c++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
