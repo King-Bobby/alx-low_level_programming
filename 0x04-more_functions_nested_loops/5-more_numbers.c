26d02e1557d44f407d5c5aa5088acb650edaef51
Noel Brown <noelbrowndboss@gmail.com>
2022-12-15 09:17:49 -0800

    print numbers from 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times, numbers from 0-14
+ * @n: number of times
+ * @i: numbers
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int n;
+	int i;
+	
+	for (n = 48; n < 58; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				putchar((i / 10) + 48);
+			}
+			putchar((i % 10) + 48);
+		}
+		putchar('\n');
+	}
+}
+
