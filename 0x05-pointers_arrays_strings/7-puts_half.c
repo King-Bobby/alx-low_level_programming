7d75aa61afe88cc0d59d0ce2cc175cc8d9b113e4
Noel Brown <noelbrowndboss@gmail.com>
2022-12-19 15:27:19 -0800

    other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string.
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int a = 0;
+	int n;
+	
+	while (a >= 0)
+	{
+		if (str[a] == '\0')
+			break;
+		a++;
+	}
+	if (a % 2 == 1)
+	{
+		n = a / 2;
+	}
+	else
+	{
+		n = (a - 1) / 2;
+	}
+	for (n++; n < a; n++)
+	{
+		putchar(str[n]);
+	}
+	putchar('\n');
+}
