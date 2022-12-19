14292bab6e0528432681d2eca13a130e567191bb
Noel Brown <noelbrowndboss@gmail.com>
2022-12-19 14:51:25 -0800

    rverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strlen -  length of string
+ * @s: string
+ * return: length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+	
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
+
+/**
+ * rev_string -  print a string and its reverse
+ * @s: string
+ * return: nothing
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+	int b = (_strlen(s) - 1);
+	char c;
+
+	while (a < b)
+	{
+		c = s[a];
+		s[a] = s[b];
+		s[b] = c;
+		a++;
+		--b;
+	}
+}
