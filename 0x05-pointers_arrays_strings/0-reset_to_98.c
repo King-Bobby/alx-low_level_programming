466f53c024a503fec5f4171da215897986b17a77
Noel Brown <noelbrowndboss@gmail.com>
2022-12-19 13:28:39 -0800

    update pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * rest_to_98 - changes the value of the pointer
+ * @*n: pointer
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A folder that contains exercises on how to use pointers, arrays and strings effectively
