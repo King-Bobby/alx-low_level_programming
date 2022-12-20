69a1d178e479264e2fc766c50f703424b90bd777
Noel Brown <noelbrowndboss@gmail.com>
2022-12-19 16:34:39 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, a = 0, b = 1, c = 1, i;
-	
+
 	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
