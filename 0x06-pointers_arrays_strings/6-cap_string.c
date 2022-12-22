2465733e3e04f6bf403080d32cf26d4ed3f554e0
Noel Brown <noelbrowndboss@gmail.com>
2022-12-22 12:38:19 -0800

    capitalize first letter of words in string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize string
+ * @s: string
+ * Return: Output
+ */
+char *cap_string(char *s)
+{
+	int a = 0;
+
+	if (s[a] >= 97 && s[a] <= 122)
+	{
+		s[a] = s[a] - 32;
+	}
+	a++;
+
+	while (s[a] != '\0')
+	{
+		if ((s[a] >= 97 && s[a] <= 122) && (s[a - 1] == ',' || s[a - 1] == ';' || s[a-1] == '.' || s[a - 1] == '!' || s[a - 1] == '?' || s[a - 1] == '"' || s[a - 1] == '(' || s[a - 1] == ')' || s[a - 1] == '{' || s[a - 1] == '}' || s[a -1] == ' ' || s[a -1 ] == '\t' || s[a -1] == '\n'))
+		{
+			s[a] = s[a] - 32;
+		}
+		a++;
+	}
+	return (s);
+}
