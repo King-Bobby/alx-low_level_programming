4088b2215f6c629d79512c4826d1a543e6402461
Noel Brown <noelbrowndboss@gmail.com>
2023-01-02 12:46:44 -0800

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets a pointer to a pointer
+ * @s: second pointer
+ * @to: first pointer
+ * Return: Always 0;
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
