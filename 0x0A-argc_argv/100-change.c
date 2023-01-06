8cdfb97e3f4c607ad5c5f900cf0a2b085b4f7379
Noel Brown <noelbrowndboss@gmail.com>
2023-01-06 11:46:23 -0800

    change cents

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int c;
+	int n = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	c = atoi(argv[1]);
+	n += c / 25;
+	c = c % 25;
+	n += c / 10;
+	c = c % 10;
+	n += c / 5;
+	c = c % 5;
+	n += c / 2;
+	c = c % 2;
+	n += c / 1;
+		
+	printf("%d\n", n);
+	return (0);
+}
