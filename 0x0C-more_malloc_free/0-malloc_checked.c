0db2430e32945c7aaa34f3eccfc92cc1bca4b866
Noel Brown <noelbrowndboss@gmail.com>
2023-01-12 15:03:52 -0800

    allocates memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,13 +2,13 @@
 
 /**
  * malloc_checked - cause normal process termination with 98
- * @b - size of bytes
+ * @b: size of bytes
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *mem;
-
+	
 	mem = malloc(b);
 	if (mem == NULL)
 	{
