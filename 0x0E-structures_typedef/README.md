bbc0bb8d27aa02893941cf7becfa50a7a65739c5
Noel Brown <noelbrowndboss@gmail.com>
2023-01-16 11:08:02 -0800

    Structs

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+A folder containing exercises on Structs and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * struct my_dog - gives description of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}my_dog;
