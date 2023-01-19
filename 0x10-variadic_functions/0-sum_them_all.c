8cdeffaa3a5a997df1467ba632f7baa23f76db96
Noel Brown <noelbrowndboss@gmail.com>
2023-01-19 12:50:22 -0800

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all -return the sum of all its arguments
+ * @n: numbers
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int sum = 0, i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	
+	va_start(list, n);
+	
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end (list);
+	return (sum);
+}
