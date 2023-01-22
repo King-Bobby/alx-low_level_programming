2bc0492876e727395edd4113ee084ddd0a76aef2
Noel Brown <noelbrowndboss@gmail.com>
2023-01-22 12:35:04 -0800

    betty style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,13 +15,13 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		return (0);
 	}
-	
+
 	va_start(list, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
 	}
-	va_end (list);
+	va_end(list);
 	return (sum);
 }
