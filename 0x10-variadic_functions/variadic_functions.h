dd5db86abe2c8aafe828e61c2a6808910a44bdcc
Noel Brown <noelbrowndboss@gmail.com>
2023-01-23 07:02:28 -0800

    sum

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,10 +9,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct data - has all the data types
+ * @identifier: to identify the function to be used 
+ * @func: function pointer
+ */
 typedef struct data
 {
 	char identifier;
 	void (*func)(va_list);
-}datatype;
+} datatype;
 
 #endif
