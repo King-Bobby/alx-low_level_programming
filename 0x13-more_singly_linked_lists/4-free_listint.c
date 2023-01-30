fe05fff994c74d8c7aefe4cb65ed156d41f82a1d
Noel Brown <noelbrowndboss@gmail.com>
2023-01-30 15:51:28 -0800

    frees a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: head of the list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
