0d841032510ad523d5e074c11e003583ac96aca9
Noel Brown <noelbrowndboss@gmail.com>
2023-01-30 15:05:27 -0800

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+A folder with more exercises on Single Linked Lists
