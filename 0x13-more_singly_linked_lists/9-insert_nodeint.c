22fc7cccd0cc17469b893939fbe08b666c1ca7b3
Noel Brown <noelbrowndboss@gmail.com>
2023-01-30 16:58:43 -0800

    insert new node at given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	temp = *head;
-	while (i < (index - 1))
+	while (i < (idx - 1))
 	{
 		temp = temp->next;
 		i++;
