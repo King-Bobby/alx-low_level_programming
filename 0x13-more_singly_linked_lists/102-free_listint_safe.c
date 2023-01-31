5fac1fad3280dab0f0634a21f0cc76474c706cbd
Noel Brown <noelbrowndboss@gmail.com>
2023-01-31 15:03:24 -0800

    frees list (safe version)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free-listint_safe - frees a listint_t list
+ * @h: head of the list
+ * Return: the size of the list that was free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp, *temp2;
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+	
+	temp = *h;
+	while (temp != NULL)
+	{
+		i++;
+		temp2 = temp;
+		temp = temp->next;
+		free(temp2);
+		if (temp2 < temp)
+			break;
+		*h = NULL;
+	}
+	return (i);
+}
