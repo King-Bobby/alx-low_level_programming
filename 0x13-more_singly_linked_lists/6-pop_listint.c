f1a18ac2c271c769f7bb43050d17eda6e11afcaf
Noel Brown <noelbrowndboss@gmail.com>
2023-02-01 14:50:53 -0800

    betty style changes

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a list 
+ * pop_listint - deletes the head node of a list
  * @head: head of the list
  * Return: the head node's data
  */
@@ -9,10 +9,10 @@ int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	int data;
-	
+
 	if (*head == NULL)
 		return (0);
-	
+
 	temp = *head;
 	data = temp->n;
 	*head = temp->next;
