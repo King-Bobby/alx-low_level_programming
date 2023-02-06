83b0b9cc46665280a4fe2318b5993562c1f1eb1e
Noel Brown <noelbrowndboss@gmail.com>
2023-02-06 12:04:58 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * create_file - creates a file
@@ -9,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, _nwrite;
+	int fd, _nwrite, len;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,7 +19,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-	_nwrite = write(fd, text_content, strlen(text_content));
+	for (len = 0; text_content[len]; len++);
+	_nwrite = write(fd, text_content, len);
 	if (_nwrite == -1)
 		return (-1);
 	close(fd);
