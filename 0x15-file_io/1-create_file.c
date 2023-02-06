81057cf66779b2401ed017a767a9922e9e781199
Noel Brown <noelbrowndboss@gmail.com>
2023-02-06 14:37:21 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,7 @@ int create_file(const char *filename, char *text_content)
 
 	for (len = 0; text_content[len]; len++)
 		;
+	len = len - 1;
 	_nwrite = write(fd, text_content, len);
 	if (_nwrite == -1 || _nwrite != len)
 	{
