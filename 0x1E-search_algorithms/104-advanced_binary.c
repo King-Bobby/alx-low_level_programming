9d690207c10493a0fdff2beb7c3916b3bc2c5bd0
Noel Brown <noelbrowndboss@gmail.com>
2023-07-13 05:05:02 -0700

    advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -29,10 +29,10 @@ int bi_search(int *array, size_t left, size_t right, int value)
 
 		if (array[mid] == value)
 		{
-			if (mid > left && array[mid - 1] == value)
-				return (bi_search(array, left, mid, value));
-			else
+			if (mid == 0 || array[mid - 1] != value)
 				return (mid);
+			else
+				return (bi_search(array, left, mid, value));
 		}
 		else if (array[mid] < value)
 			return (bi_search(array, mid + 1, right, value));
